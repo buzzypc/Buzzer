2db4549dd247d52a847ff6a9ca003a959935a2d4
buzzypc123@gmail.com <buzzypc123@gmail.com>
2024-07-27 14:10:32 +0530

    check for call failures

--- static/master-script.js
+++ static/master-script.js
@@ -1,21 +1,20 @@
-document.getElementById('toggle-div').addEventListener('click', function() {
+document.getElementById('toggle-div').addEventListener('click', function () {
   const buzzer = document.getElementById('buzzer-sound');
-    document.body.classList.toggle('blue-background');
-    buzzer.currentTime = 0;
-  buzzer.play();
-    fetch('/reset', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json'
-      },
-      body: JSON.stringify({ ip: clientIp })
-    })
+  fetch('/reset', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({ ip: clientIp })
+  })
     .then(response => response.json())
     .then(data => {
       console.log('Success:', data);
+      document.body.classList.toggle('toggle-background');
+      buzzer.currentTime = 0;
+      buzzer.play();
     })
     .catch((error) => {
       console.error('Error:', error);
     });
-  });
-  
+});
--- static/script.js
+++ static/script.js
@@ -1,8 +1,5 @@
 document.getElementById('toggle-div').addEventListener('click', function () {
   const buzzer = document.getElementById('buzzer-sound');
-  document.body.classList.toggle('blue-background');
-  buzzer.currentTime = 0;
-  buzzer.play();
   fetch('/submit', {
     method: 'POST',
     headers: {
@@ -13,7 +10,9 @@ document.getElementById('toggle-div').addEventListener('click', function () {
     .then(response => response.json())
     .then(data => {
       console.log('Success:', data);
-      
+      document.body.classList.toggle('toggle-background');
+      buzzer.currentTime = 0;
+      buzzer.play();
     })
     .catch((error) => {
       console.error('Error:', error);
